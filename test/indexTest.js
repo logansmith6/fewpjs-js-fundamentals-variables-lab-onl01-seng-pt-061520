ec4fc669a352542d1a2d59a0df34f8bd9ab290a5
Logan Smith <logsmith06@gmail.com>
2020-12-09 17:34:24 +0000

    Done.

--- test/indexTest.js
+++ test/indexTest.js
@@ -8,7 +8,7 @@ const js = fs.readFileSync(path.resolve(__dirname, '..', 'index.js'), 'utf-8')
 describe('index.js', function () {
   describe('companyName', function () {
     it('is set as Scuber', function () {
-      expect(companyName).to.equal('Scuber');
+       expect(companyName).to.equal('Scuber');
     });
 
     it('is defined as a const', function () {
